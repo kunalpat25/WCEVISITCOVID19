b50e12957907bae4e97a165d6f9fb3f0e69abfe4
Kunal Patrikar <kunalpatrikar25@gmail.com>
2020-10-30 14:35:45 +0530

    Added exit feature on Exit button click and onBackPress
    Fixed - ProgressBar keeps rotating on MainActivity even NormalUser is logged in

--- app/src/main/java/com/wce/wcevisitcovid19/GuidelinesActivity.java
+++ app/src/main/java/com/wce/wcevisitcovid19/GuidelinesActivity.java
@@ -3,6 +3,9 @@ package com.wce.wcevisitcovid19;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ScrollView;
 
 public class GuidelinesActivity extends AppCompatActivity {
 
@@ -10,5 +13,31 @@ public class GuidelinesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guidelines);
+
+        ScrollView scrollView = findViewById(R.id.scroll_view);
+        scrollView.scrollTo(0,scrollView.getBottom());
+
+        Button btnExit = findViewById(R.id.btn_exit);
+        btnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                moveTaskToBack(true);
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        moveTaskToBack(true);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        ScrollView scrollView = findViewById(R.id.scroll_view);
+        scrollView.scrollTo(0, scrollView.getTop());
     }
 }
